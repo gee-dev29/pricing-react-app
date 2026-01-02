88913742a4eb6da4438766440048f2eb78850a5f
Godwin Pro <maduegodwin2605@gmail.com>
2026-01-02 11:46:47 +0100

    fixed

--- react-project1/src/App.jsx
+++ react-project1/src/App.jsx
@@ -1,19 +1,31 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
+import SignUp from "./components/SignUp";
 
 function App() {
     return (
         <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
-            <Navbar />
-            <Hero />
-            <Features />
-            <Pricing />
-            <Testimonials />
-            <Footer />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <Navbar />
+                            <Hero />
+                            <Features />
+                            <Pricing />
+                            <Testimonials />
+                            <Footer />
+                        </>
+                    }
+                />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
         </div>
     );
 }
